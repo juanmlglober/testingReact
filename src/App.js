496fc13bb34756eafa2c9036b7c55be336fbc030
Juan Moreno <juanignacio.moreno@globant.com>
2022-09-19 09:48:45 -0300

    feat(app): add funcionality and test working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
+  const [buttonColor, setButtonColor] = useState("red")
+  const newButtonColor = buttonColor === "red" ? "blue" : "red"
+  const handleButton = () => setButtonColor(newButtonColor)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button style={{ backgroundColor: buttonColor }} onClick={handleButton}>
+        Change to {newButtonColor}
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
